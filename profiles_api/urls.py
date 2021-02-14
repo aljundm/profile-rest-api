d5a393541849404e7900cf935f3e1190c02612e3
aljundm <aljundm88@gmail.com>
2021-02-14 14:05:37 +0800

    fixed
    fixed

---
+++ profiles_api/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import url, include
+
+from rest_framework.authtoken import views as auth_views
+from rest_framework.routers import DefaultRouter
+
+from . import views
+
+router = DefaultRouter()
+router.register('profile', views.UserProfileViewSet)
+router.register(
+    'hello-viewset', views.HelloViewSet, base_name="hello-view-set")
+
+urlpatterns = [
+    url(r'^hello-view/', views.HelloApiView.as_view()),
+    url(r'', include(router.urls)),
+]
